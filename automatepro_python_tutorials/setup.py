789b5ce8c2354c9cb5a84bba9d6a4def9d436cc3
Simone Contorno <simone.contorno@lemvos.com>
2026-09-22 13:49:00 +0200

    build: drop unused dependencies and dead config
    - Both packages now depend only on the message packages their sources
      include: sensor_msgs and automatepro_interfaces.
    - Remove std_srvs, std_msgs, geometry_msgs, and ublox_msgs, the missing
      include/ directory, the commented-out installs, and the setup.py
      installs for launch/ and config/, which do not exist.

--- automatepro_python_tutorials/setup.py
+++ automatepro_python_tutorials/setup.py
@@ -1,5 +1,3 @@
-import os
-from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'automatepro_python_tutorials'
@@ -12,8 +10,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
